de4a1933a0758a50ae99bd822267c4ea2080d2d5
lucasHelio <lucasheliosr@hotmail.com>
2021-04-30 10:41:38 -0300

    ordenação implementada

--- queues.c
+++ queues.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 typedef struct
 {
@@ -14,79 +15,22 @@ typedef struct
 }ListaQueue;
 
 
+
 void deQueue(ListaQueue *lista) //o team queue eu nao removo
 {
     printf("%d\n", lista->elemento[0]);
     for(int i=0; i<lista->contadorElementos-1;i++)
     lista->elemento[i]=lista->elemento[i+1];
-    lista->contadorElementos-=1;
+    lista->contadorElementos=lista->contadorElementos - 1;
+    return;
 }
 
-/*
-void enQueue(ListaQueue *lista, int valor)
+
+
+void enQueue(ListaQueue *lista, ListaTimes *times, int valor, int numerodeTimes)
 {
-    for(int z=0;z<= lista->contadorElementos;z++)
-    {
-        if (z==0 && lista->contadorElementos==0) //primeiro elemento adicionado
-        {
-            lista->elemento[z]=valor;
-            lista->contadorElementos++;
-            break;
-        }
+    
 
-        else if (lista->contadorElementos==1 && valor > lista->elemento[0]) //so cai aqui quando tem 1 elemento e 
-        {                                                                        //valor é maior que elemento[0]
-            lista->elemento[1]=valor;
-            lista->contadorElementos++;
-            break;
-        }
-        
-        else if (lista->contadorElementos==1 && valor < lista->elemento[0]) //so cai aqui quando tem 1 elemento e
-        {                                                                           //valor é menor que elemento[0]
-            
-            
-            lista->elemento[1]=lista->elemento[0];
-            lista->elemento[0]= valor;
-            lista->contadorElementos++;
-            break;
-        }
-
-        else if (lista->contadorElementos>=2 && valor>=lista->elemento[z] && valor<lista->elemento[z+1] && z<=lista->contadorElementos-2) 
-        {                                                         //so cai aqui quando tem 2 elementos ou mais,  x <= valor < y
-
-            for (int f=lista->contadorElementos; f > z+1;f--) //acho que seria f> z+1
-            {
-                printf("contador de elementos antes de entrar no loop: %d\n", lista->contadorElementos);
-                lista->elemento[f] = lista->elemento[f-1];
-                break;
-            }
-            lista->elemento[z+1] = valor;
-            lista->contadorElementos++;
-            //break;
-        }
-        
-        else if(lista->contadorElementos>=2 && valor<lista->elemento[0])//menor dq o primeiro elemento
-        {
-            for(int f=lista->contadorElementos; f>1; f--)
-            {
-                lista->elemento[f]=lista->elemento[f-1];
-            }
-            lista->elemento[0]=valor;
-        }
-        
-        else
-        {
-            lista->elemento[lista->contadorElementos]=valor;
-            lista->contadorElementos++;
-        }
-    }
-    //lista->elemento[lista->contadorElementos]=valor; //por enquanto adiciona no final da lista
-    //lista->contadorElementos++;
-    //return;
-}*/
-
-void enQueue(ListaQueue *lista, int valor)
-{
     if (lista->contadorElementos == 0)  //primeira adicao
     {
         lista->elemento[0]=valor;
@@ -94,31 +38,53 @@ void enQueue(ListaQueue *lista, int valor)
         return;
     }
 
-    else if(valor<=lista->elemento[lista->contadorElementos-1]) //caso valor seja menor ou igual ao ultimo elemento
-    {
-        
-        for(int i=0;i<lista->contadorElementos;i++)
+    
+   else if(lista->contadorElementos>=1)
+   {
+        int indexNum=0, maiorIndex=0, indexTime, achouElem=0;
+        for(int i=0; i< numerodeTimes;i++)
         {
-
-            if(valor <= lista->elemento[i])
+            for(int j=0;j<times[i].contadorElementos;j++)
             {
-                for(int j=lista->contadorElementos;j>=i+1;j--)
+                if(valor == times[i].elemento[j])
                 {
-                    lista->elemento[j]=lista->elemento[j-1];
+                    indexTime=i;
+                    break;
+                    //if (indexNum>=maiorIndex)maiorIndex=indexNum;
                 }
-                lista->elemento[i]=valor;
-                lista->contadorElementos++;
-                return;
             }
         }
-        
+        for(int q=0;q<lista->contadorElementos;q++)
+        {
+            for(int t=0;t<times[indexTime].contadorElementos;t++)
+            {
+                if(lista->elemento[q]== times[indexTime].elemento[t])
+                {
+                    achouElem++;
+                    indexNum=q;
+                    if (indexNum>=maiorIndex)maiorIndex=indexNum;
+                }
+            }
+        }
+
+        if(achouElem>0)
+        {
+            for(int p=lista->contadorElementos;p>maiorIndex+1;p--)
+            {
+                lista->elemento[p]=lista->elemento[p-1];
+            }
+            lista->elemento[maiorIndex+1]=valor;
+            lista->contadorElementos++;
+            return;
+        }
+        else
+        {
+            lista->elemento[lista->contadorElementos]=valor;
+            lista->contadorElementos++;
+            return;
+        }
     }
-    else        //caso valor seja maior dq o ultimo elemento
-    {
-        lista->elemento[lista->contadorElementos]= valor;
-        lista->contadorElementos++;
-        return;
-    }
+   
 
 }
 
@@ -191,7 +157,7 @@ int main()
                 else if (strcmp(comando, "ENQUEUE")== 0) //adiciona na lista
                 {
                     scanf("%d", &elementoN);
-                    enQueue(&lista, elementoN);
+                    enQueue(&lista, &times, elementoN, numerodeTimes);
                     
                 }
 
